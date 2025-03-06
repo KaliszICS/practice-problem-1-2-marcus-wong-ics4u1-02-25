2818c4daab640b711c4d8fbb5226413e66290d06
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-03-06 14:40:50 +0000

    same assignment 1.2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,35 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static boolean validIndex(int[] intArr, int num) {
+		try {
+			int temp = intArr[num];
+			return true;
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+
+		}
+
+	}
+	public static int divide(int num2, int num3) {
+		try {
+			return num2 / num3;
+		}
+		catch (ArithmeticException e) {
+			return 0;
+		}
+	}
+	public static int safeConvertStringtoInt(String word) {
+		try {
+			return Integer.parseInt(word);
+		}
+		catch (NumberFormatException e) {
+			return 0;
+		}
+		
+
+	}
 
 	
 
